b0a4e24545e532115783376e7183874cd9993699
akniyett <60417824+akniyett@users.noreply.github.com>
2021-02-24 11:47:16 +0600

    lab

--- laab4/src/app/product-details/product-details.component.ts
+++ laab4/src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,9 +12,23 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
 
-  constructor(private route: ActivatedRoute,) { }
+  
+
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(product => product.id === productIdFromRoute);
+    
+  }
+  share() {
+    window.alert('The product has been shared!');
   }
 
 }
--- laab4/src/app/products.ts
+++ laab4/src/app/products.ts
@@ -1,20 +1,112 @@
 export const products = [
-    {
-      id: 1,
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      id: 2,
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
-    }
-  ];
+  {
+    id: 1,
+    name: 'HP 24mh FHD Monitor',
+    ratings: 9.445,
+    price: 169.99,
+    url:'https://www.amazon.com/HP-24mh-FHD-Monitor-Built/dp/B08BF4CZSV/ref=sr_1_2?dchild=1&keywords=HP+24mh+FHD+Monitor&qid=1614136923&sr=8-2',
+    description: 'Computer Monitor with 23.8-Inch IPS Display (1080p) - Built-In Speakers and VESA Mounting - Height/Tilt Adjustment for Ergonomic Viewing - HDMI and DisplayPort - (1D0J9AA#ABA)',
+    img1:'https://images-na.ssl-images-amazon.com/images/I/91fAU6mxFsL._AC_SL1500_.jpg',
+    img2:'https://images-na.ssl-images-amazon.com/images/I/81zsiJ%2BJnfL._AC_SL1500_.jpg',
+    img3:'https://images-na.ssl-images-amazon.com/images/I/81Tte45ADrL._AC_SL1500_.jpg'
+  },
+  {
+    id: 2,
+    name: 'Dual Monitor Stand',
+    ratings: 206,
+    price: 99.99,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/717TW1hyqVL._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/71zBequCKEL._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/713U8ZZc1iL._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/Dual-Monitor-Stand-Computer-Notebooks/dp/B08DNTVY8Y/ref=sr_1_5?dchild=1&keywords=Dual+Monitor+Stand+Gas+Spring+Monitor&qid=1614137032&sr=8-5',
+    description: 'Gas Spring Monitor Mount with Laptop Tray Fit Two 13 to 27 Inch Flat Curved Computer Screens and 10 to 17 Inch Notebooks with C Clamp, Grommet Mounting Base'
+  },
+  {
+    id: 3,
+    name: 'Video Conference Lighting',
+    ratings: 89,
+    price: 27.99 ,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/71S6r7M8WuL._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/81U8ggnz8JL._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/71kQDadXLRL._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/Conference-Lighting-Conferencing-Dimmable-Monitor/dp/B08PYY2TCC/ref=sr_1_10?dchild=1&keywords=Video+Conference+Lighting&qid=1614137086&sr=8-10',
+    description: 'Selfie Ring Light with Clamp Mount for Video Conferencing,Webcam Light with 3 Light Modes&6 Level Dimmable for Laptop/PC Monitor/Desk/Bed/Office/Makeup/YouTube/TIK Tok'
+  },
+  {
+    id: 4,
+    name: 'APEMAN Dash Cam',
+    ratings: 240,
+    price: 119.99,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/81hbuFxEzfL._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/8196u682XAL._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/71siESRqYwL._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/APEMAN-Support-G-Sensor-Recording-Optional/dp/B08KCWWZW9/ref=sr_1_9?dchild=1&keywords=APEMAN+Dash+Cam&qid=1614137132&sr=8-9',
+    description: 'Included 32GB SD Card, 1080P Dual Dash Cam Front and Rear, Car Camera with Dual 170 ° Wide Angle, Night Vision, 24hr Parking Monitor, Motion Detection, Loop Recording'
+  },
+  {
+    id: 5,
+    name: 'BOSOBO Paint Brushes Set',
+    ratings: 21689,
+    price: 6.99,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/81ON4-sLO0L._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/81TR2fj-dqL._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/81xd5Nc8OyL._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/BOSOBO-Paintbrushes-Watercolor-Miniature-Detailing/dp/B07GH7WGC3/ref=sr_1_3?dchild=1&keywords=BOSOBO+Paint+Brushes+Set&qid=1614137217&sr=8-3',
+    description: '2 Pack 20 Pcs Round Pointed Tip Paintbrushes Nylon Hair Artist Acrylic Paint Brushes for Acrylic Oil Watercolor, Face Nail Art, Miniature Detailing & Rock Painting, Blue'
+  },
+  {
+    id: 6,
+    name: 'ARTEGRIA Detail Paint Brush Set',
+    ratings: 314,
+    price: 9.99,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/810dFpUsorL._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/719%2Btg-sh4L._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/719hHgw2XNL._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/ARTEGRIA-Detail-Paint-Brush-Set/dp/B085WWRF2T/ref=sr_1_3?dchild=1&keywords=ARTEGRIA+Detail+Paint+Brush+Set&qid=1614137258&sr=8-3',
+    description: '5 Miniature Paint Brushes Size Round 3/0 000 with Fine Tips, Ergonomic Handle for Small Scale Army Model Art and Paint by Numbers for Adults - Acrylic Watercolor Oil'
+  },
+  {
+    id: 7,
+    name: 'Face Masks',
+    ratings: 148789,
+    price: 9.99,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/71RPy4MEIDL._AC_SL1200_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/61UCWErmFaL._AC_SL1200_.jpgg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/71sa8bASJUL._AC_SL1200_.jpg',
+    url: 'https://www.amazon.com/Disposable-Breathable-Non-woven-Comfortable-Allergens%EF%BC%8820PCS%EF%BC%89/dp/B0859RSTXN/ref=sr_1_41?dchild=1&keywords=face+masks+wecolor+100pcs+disposable&qid=1614137415&sr=8-41',
+    description: 'Wecolor 100 Pcs Disposable 3 Ply Earloop Face Masks, Suitable for Home, School, Office and Outdoors (Blue)'
+  },
+  {
+    id: 8,
+    name: 'Video Conference Lighting',
+    ratings: 89,
+    price: 1199.00,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/813KcWFAaYL._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/81aUazqOs2L._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/714W47RPJ-L._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/Owl-Labs-Meeting-Premium-Pack/dp/B084C5HG1F/ref=sr_1_1_sspa?dchild=1&keywords=Video+Conference+Lighting&qid=1614137555&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzRlpTMTQzUzZLWkVYJmVuY3J5cHRlZElkPUEwOTk2NjgzNUFaUUxORElCM0dRJmVuY3J5cHRlZEFkSWQ9QTA4NTY2OTROSEI0RkQxSTAyMUYmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl',
+    description: 'Owl Labs Meeting Owl Pro Premium Pack - 360 Degree, 1080p Smart Video Conference Camera, Microphone, and Speaker (Includes Accessories and Warranty)'
+  },
+  {
+    id: 9,
+    name: 'Led Strip Lights',
+    ratings: 29178,
+    price: 13.59,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/71LjnFGCK2L._AC_SL1200_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/71bD-a5DieL._AC_SL1200_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/71IhChrldkL._AC_SL1200_.jpg',
+    url: 'https://www.amazon.com/Backlight-6-1ft-13ft-32-70in-Thousands-Changing/dp/B07Z9YLRD7/ref=sr_1_58_sspa?dchild=1&keywords=Led+Strip+Lights&qid=1614137625&sr=8-58-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyV0xDM1NMUTc5MDJEJmVuY3J5cHRlZElkPUEwNzk1NjMxVUo4STExTDJHRVVaJmVuY3J5cHRlZEFkSWQ9QTA5Mjc1ODUxOFlFMVNXSjRMOFlSJndpZGdldE5hbWU9c3BfYnRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==',
+    description: 'PANGTON VILLA Led Strip Lights 6.56 Feet for 40-60 Inch TV Usb Backlight Kit with Remote Rgb 16 Colors Bias Mood Lamp for Bedroom, Room Hdtv'
+  },
+  {
+    id: 10,
+    name: 'Ultrean Air Fryer',
+    ratings: 14486,
+    price: 59.49,
+    img1: 'https://images-na.ssl-images-amazon.com/images/I/71rmONH81iL._AC_SL1500_.jpg',
+    img2: 'https://images-na.ssl-images-amazon.com/images/I/71ZZhReuMeL._AC_SL1500_.jpg',
+    img3: 'https://images-na.ssl-images-amazon.com/images/I/71oNUPKkPBL._AC_SL1500_.jpg',
+    url: 'https://www.amazon.com/Ultrean-Airfryer-Programmable-Detachable-Anti-scratch/dp/B07ZNDQQDM/ref=sr_1_3?dchild=1&keywords=Ultrean%2BAir%2BFryer%2B4.2%2BQuart%2B(4%2BLiter)%2BElectric%2BHot%2BAir%2BFryers%2BOven%2BOilless&qid=1614137811&sr=8-3&th=1',
+    description: '4.2 Quart (4 Liter) Electric Hot Air Fryers Oven Oilless Cooker with LCD Digital Screen and Nonstick Frying Pot, UL Certified, 1-Year Warranty, 1500W (Red)'
+  }
+];
